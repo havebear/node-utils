fa5e73c16598fac36642286aa6506efdbb7df6a5
xbb <616749285@qq.com>
2025-04-03 16:43:08 +0800

    feat: 1.9

--- src/World/World.js
+++ src/World/World.js
@@ -3,6 +3,7 @@ import { createCube } from './components/cube.js';
 import { createLights } from './components/lights.js'
 import { createScene } from './components/scene.js';
 
+import { createControls } from './systems/controls.js';
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/Resizer.js';
 import { Loop } from './systems/Loop.js';
@@ -26,6 +27,8 @@ class World {
         renderer = createRenderer();
         loop = new Loop(camera, scene, renderer);
         container.append(renderer.domElement);
+
+        const controls = createControls(camera, renderer.domElement);
 
         const cube = createCube();
         const cube2 = createCube();
---
+++ src/World/systems/controls.js
@@ -0,0 +1,15 @@
+// import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from '../../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+// console.log(OrbitControls)
+
+function createControls (camera, canvas) {
+    // const controls = new OrbitControls(camera, canvas);
+
+    // return controls
+    return {}
+}
+
+export {
+    createControls
+}
